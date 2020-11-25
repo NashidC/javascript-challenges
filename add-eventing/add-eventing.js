6a920ad03889951c7f2214de0c213c616493629f
NashidC <nash.chowdhury@gmail.com>
2020-11-25 01:13:14 -0500

    hw complete

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,21 @@
-const addEventing = function (obj) {
-}
+
+const addEventing = (obj) => {
+  const eventings = [];
+  let objReturn = {
+    on: (event, callBack) => {
+      if (!eventings[event]) {
+        eventings[event] = [];
+      }
+      eventings[event].push(callBack);
+    },
+
+    trigger: (event, ...args) => {
+      if (eventings[event]) {
+        eventings[event].map((callback) => callback(...args));
+      }
+    },
+  };
+  return objReturn;
+};
 
 module.exports = addEventing
--- target-sum-exists/target-sum-exists.js
+++ target-sum-exists/target-sum-exists.js
@@ -1,10 +1,25 @@
 const targetSumExistsInTwoNumbers = (list, target) => {
-  return false
-}
+  for (let i in list) {
+    for (let j in list) {
+      if (list[i] + list[j] === target) {
+        return true;
+      }
+    }
+  }
+};
 
 const targetSumExistsInThreeNumbers = (list, target) => {
-  return false
-}
+
+  for (let i in list) {
+    for (let j in list) {
+      if (list[i] + list[j] === target) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
 
 module.exports = {
   targetSumExistsInTwoNumbers,
